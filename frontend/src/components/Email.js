6d3fc87bf865e9dee358d0db2a83ce18bce81d92
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 02:06:06 -0500

    Update Email.js

--- frontend/src/components/Email.js
+++ frontend/src/components/Email.js
@@ -1,9 +1,27 @@
 import React from 'react';
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport(
+    {
+        service: 'gmail', // e.g., 'Gmail', 'SendGrid'
+        auth: 
+        {
+          user: 'CARDINALCCVerify@example.com',
+          pass: 'COP4331-14',
+        },
+    });
+
+    const mailOptions = 
+{
+    from: 'CARDINALCCVerify',
+    to: email,
+    subject: 'Email Verification Code',
+    text: `Your verification code is: ${vernum}`,
+ };
+
 
 function EmailNode()
 {
-    
-const nodemailer = require('nodemailer');
 var _ud = localStorage.getItem('user_data');
 var ud = JSON.parse(_ud);
 var firstName = ud.firstName;
@@ -11,30 +29,41 @@ var lastName = ud.lastName;
 var email = ud.email;
 const vernum = Math.floor(Math.random() * 100000);
 
+async function sendEmail() {
+    let transporter = nodemailer.createTransport(
+        {
+            service: 'gmail', // e.g., 'Gmail', 'SendGrid'
+            auth: 
+            {
+              user: 'CARDINALCCVerify@example.com',
+              pass: 'COP4331-14',
+            },
+        });
+
+        const mailOptions = 
+        {
+            from: 'CARDINALCCVerify',
+            to: email,
+            subject: 'Email Verification Code',
+            text: `Your verification code is: ${vernum}`,
+         };
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: %s', info.messageId);
+    } catch (error) {
+        console.error('Error sending email: ', error);
+    }
+}
+sendEmail();
+
+
 const doLogout = event =>
 {
 event.preventDefault();
 localStorage.removeItem("user_data")
 window.location.href = '/';
 };
-
-const transporter = nodemailer.createTransport(
-{
-    service: 'gmail', // e.g., 'Gmail', 'SendGrid'
-    auth: 
-    {
-      user: 'CARDINALCCVerify@example.com',
-      pass: 'COP4331-14',
-    },
-});
-
-const mailOptions = 
-{
-    from: 'CARDINALCCVerify',
-    to: email,
-    subject: 'Email Verification Code',
-    text: `Your verification code is: ${vernum}`,
- };
 
 
 return(
