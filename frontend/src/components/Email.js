8b487d0f6ae6e536b3733c2d5ac91fc230b9f9a1
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 02:08:38 -0500

    Update Email.js

--- frontend/src/components/Email.js
+++ frontend/src/components/Email.js
@@ -1,23 +1,4 @@
 import React from 'react';
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport(
-    {
-        service: 'gmail', // e.g., 'Gmail', 'SendGrid'
-        auth: 
-        {
-          user: 'CARDINALCCVerify@example.com',
-          pass: 'COP4331-14',
-        },
-    });
-
-    const mailOptions = 
-{
-    from: 'CARDINALCCVerify',
-    to: email,
-    subject: 'Email Verification Code',
-    text: `Your verification code is: ${vernum}`,
- };
 
 
 function EmailNode()
@@ -27,36 +8,7 @@ var ud = JSON.parse(_ud);
 var firstName = ud.firstName;
 var lastName = ud.lastName;
 var email = ud.email;
-const vernum = Math.floor(Math.random() * 100000);
-
-async function sendEmail() {
-    let transporter = nodemailer.createTransport(
-        {
-            service: 'gmail', // e.g., 'Gmail', 'SendGrid'
-            auth: 
-            {
-              user: 'CARDINALCCVerify@example.com',
-              pass: 'COP4331-14',
-            },
-        });
-
-        const mailOptions = 
-        {
-            from: 'CARDINALCCVerify',
-            to: email,
-            subject: 'Email Verification Code',
-            text: `Your verification code is: ${vernum}`,
-         };
-
-    try {
-        let info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: %s', info.messageId);
-    } catch (error) {
-        console.error('Error sending email: ', error);
-    }
-}
-sendEmail();
-
+let vernum = Math.floor(Math.random() * 100000);
 
 const doLogout = event =>
 {
@@ -68,7 +20,7 @@ window.location.href = '/';
 
 return(
 <div id="loggedInDiv">
-<span id="userName">Logged In As {firstName} {lastName} {email} ${vernum}</span><br />
+<span id="userName">Logged In As {firstName} {lastName} {email}</span><br />
 <button type="button" id="logoutButton" class="buttons"
 onClick={doLogout}> Log Out </button>
 </div>
