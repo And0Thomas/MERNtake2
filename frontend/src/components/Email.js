72e33a4ed28dd16548521fca203db19401be0541
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 03:15:28 -0500

    test

--- frontend/src/components/Email.js
+++ frontend/src/components/Email.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+const nodemailer = require('nodemailer')
 
-
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user: 'CARDINALCCVerify@gmail.com',
+        pass: 'fegs wvih dqtd ccev'
+    }
+});
 
 function EmailNode()
 {
@@ -19,6 +26,17 @@ event.preventDefault();
 localStorage.removeItem("user_data")
 window.location.href = '/';
 };
+
+var message = {
+    from: "Cardinal Coffee Co" , // sender address
+to: email, // list of receivers
+subject: "Welcome Letter", // Subject line
+html: `
+<div>
+<p>Verification code ${vernum}</p>
+</div>
+`}
+transporter.sendMail(message);
 
 return(
 <div id="loggedInDiv">
