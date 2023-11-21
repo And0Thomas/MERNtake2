27c30a418bd7544c2d6a99a132e7913d9121e309
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 03:42:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const nodemailer = require("nodemailer");
 
 app.use(express.json());
 app.use(cors());
