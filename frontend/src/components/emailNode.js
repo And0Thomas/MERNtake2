d769969585137781f28b9bbe93f0dc2321206da1
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 01:41:21 -0500

    Update emailNode.js

--- frontend/src/components/emailNode.js
+++ frontend/src/components/emailNode.js
@@ -1,26 +1,8 @@
 import React from 'react';
-
-
 function emailNode()
 {
-var _ud = localStorage.getItem('user_data');
-var ud = JSON.parse(_ud);
-var firstName = ud.firstName;
-var lastName = ud.lastName;
-var email = ud.email;
-const doLogout = event =>
-{
-event.preventDefault();
-localStorage.removeItem("user_data")
-window.location.href = '/';
-};
 return(
-<div id="loggedInDiv">
-<span id="userName">Logged In As {firstName} {lastName} {email}</span><br />
-<button type="button" id="logoutButton" class="buttons"
-onClick={doLogout}> Log Out </button>
-</div>
+<h2 id="title">Hello</h2>
 );
 };
-
 export default emailNode;
