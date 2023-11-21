ebd3c332da376d6c10ce109fca824f7cd9b2b5ed
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 02:56:16 -0500

    email

--- frontend/src/components/Email.js
+++ frontend/src/components/Email.js
@@ -12,39 +12,6 @@ var lastName = ud.lastName;
 var email = ud.email;
 let vernum = Math.floor(Math.random() * 100000);
 
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "CARDINALCCVerify@gmail.com",
-      pass: "COP4331-14",
-    },
-  });
-
-  const mailOptions = {
-    from: {
-        name: "Cardinal Coffee Co",
-        address: "CARDINALCCVerify@gmail.com"
-    },
-    to: {email},
-    subject: "Email Verification CCC",
-    text: "Your Verification code is "
-  }
-
-  const sendMail = async () => {
-    try{
-        await transport.sendMail()
-        console.log("email been sent");
-    } catch (error) {
-        console.error(error)
-    }
-  }
-
-  sendMail(transporter, mailOptions);
 
 const doLogout = event =>
 {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const nodemailer = require('nodemailer');
+
 
 
 app.use(express.json());
