fc23dec628c734c8f3a10874322fad25965b2d57
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-21 00:57:56 -0500

    Create Verification.js

---
+++ frontend/src/pages/Verification.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Login from '../components/Login';
+const LoginPage = () =>
+{
+return(
+<div>
+<h1 id="title">Verification</h1>
+<PageTitle />
+<Login />
+</div>
+);
+};
+export default LoginPage;
