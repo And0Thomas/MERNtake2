7c35e549a40c877afb76b1729355bba51079cfaa
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-22 20:59:56 -0500

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
 import Hero from "../components/Hero"
-import Navbar from "../components/Navbar"
+
 import Contact from "../components/Contact"
 
 function Home() {
   return(
     <div>
-      <Navbar/>
+
       <Hero/>
       <Contact/>
     </div>
