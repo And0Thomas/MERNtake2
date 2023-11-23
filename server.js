7fada75089e6d00cac75010ed370b6d19000f002
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-22 19:47:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.set('port', (process.env.PORT || 5000));
 if (process.env.NODE_ENV === 'production')
 {
 // Set static folder
-app.use(express.static('frontend/src'));
+app.use(express.static('frontend/build'));
 app.get('*', (req, res) =>
 {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'src', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 }
 
