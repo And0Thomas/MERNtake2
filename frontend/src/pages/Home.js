0c9590ff9ee5979001b4cdeeaaf074c39f4db59e
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-22 21:02:30 -0500

    test

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
 import Hero from "../components/Hero"
-
+import Navi from "../components/Navi"
 import Contact from "../components/Contact"
 
 function Home() {
   return(
     <div>
-
+      <Navi/>
       <Hero/>
       <Contact/>
     </div>
