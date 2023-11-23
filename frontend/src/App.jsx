cd3d7a207614de3f44e84c420b71d643ab9e5d54
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-22 19:01:24 -0500

    test

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Contact from "./components/Contact"
+
+function App() {
+  return(
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
