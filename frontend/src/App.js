7b217222767467aece2ea7a087c6a860f3cabfbf
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-22 20:42:14 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
+import Home from './pages/Home';
 import CardPage from './pages/CardPage';
 import Verification from './pages/Verification';
 function App() {
 return (
 <BrowserRouter>
 <Routes>
-<Route path="/" index element={<LoginPage />} />
+<Route path="/" index element={<Home />} />
 <Route path="/cards" index element={<CardPage />} />
 <Route path="/verification" index element={<Verification />} />
 </Routes>
