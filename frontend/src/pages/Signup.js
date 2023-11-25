5819a9257fb7b46a6505b0025d60b64450a734fa
Ethan Hoang <ethanhoang2692@gmail.com>
2023-11-24 22:45:01 -0500

    test

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navi from "../components/Navi"
+import SignUp from "../components/Signup"
+
+function Signup() {
+    return(
+      <div>
+        <Navi/>
+        <SignUp/>
+      </div>
+    )
+  }
+export default Signup;
